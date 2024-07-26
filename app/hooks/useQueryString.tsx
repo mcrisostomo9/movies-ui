4a59cd244ebd8d6938e58dd21c993e53b253cdb3
Mark Crisostomo <mcrisostomo9@gmail.com>
2024-07-25 21:13:41 -0700

    fixed query string

--- app/hooks/useQueryString.tsx
+++ app/hooks/useQueryString.tsx
@@ -3,7 +3,7 @@ import { useCallback } from "react";
 
 export function useQueryString(searchParams: ReadonlyURLSearchParams) {
   const createQueryString = useCallback(
-    (name: string, value: string, deleteKey: string) => {
+    (name: string, value: string, deleteKey?: string) => {
       const params = new URLSearchParams(searchParams.toString());
       params.set(name, value);
 
